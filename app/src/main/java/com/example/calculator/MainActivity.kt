f7f2f1b6c1216ce7fa4f6d3a4defa85cdfee6480
Pedestal19 <gabeoji01@gmail.com>
2020-03-28 23:42:25 +0100

    writing click event logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun buNumberEvent(view: View) {}
+    fun buNumberEvent(view: View) {
+        val buSelect = view as Button
+        var buClickValue:String = "0"
+
+        when(buSelect.id){
+            bu0.id->{
+                buClickValue="0"
+            }
+            bu1.id->{
+                buClickValue="1"
+            }
+            bu2.id->{
+                buClickValue="2"
+            }
+            bu3.id->{
+                buClickValue="3"
+            }
+            bu4.id->{
+                buClickValue="4"
+            }
+            bu5.id->{
+                buClickValue="5"
+            }
+            bu6.id->{
+                buClickValue="6"
+            }
+            bu7.id->{
+                buClickValue="7"
+            }
+            bu8.id->{
+                buClickValue="8"
+            }
+            bu9.id->{
+                buClickValue="9"
+            }
+            buDot.id->{
+                buClickValue="."
+            }
+            buPlusMinus.id->{
+                buClickValue="+"
+            }
+        }
+    }
 }
